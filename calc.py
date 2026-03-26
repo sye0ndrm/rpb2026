adc01b3a17b1acd9f333a9ff68152aa176cca37f
sye0ndrm <st.stellae@gmail.com>
2026-03-26 22:48:43 +0900

    update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,10 @@
 def main():
 
-def add():
+def add(a, b):
+    return a + b
 
-def divide():
+def divide(x,y):
+    if y == 0:
+        print("Error: cannot divide by zero.")
+    else:
+        return x/y
